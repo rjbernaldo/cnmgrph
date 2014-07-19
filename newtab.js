6c86344a229622ad277bcc7268687e342604b965
Rj <rj.bernaldo@gmail.com>
2014-07-18 19:43:28 -0700

    Removed time function entirely

--- newtab.js
+++ newtab.js
@@ -1,36 +1,9 @@
 TIME = document.getElementById('time');
 OPA = 0;
 
-function setTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  h = changeHour(h)
-  m = mAndS(m);
-  s = mAndS(s);
-  TIME.innerHTML = h + ':' + m + ':' + s;
-}
-
-function mAndS(int) {
-  if (int < 10) {
-    int = "0" + int;
-  }
-  return int;
-}
-
-function changeHour(int){
-  if (int > 12) {
-    int = int - 12
-    int = mAndS(int);
-  }
-  return int
-}
-
 setTimeout(function() {
   TIME.setAttribute('class','show');
 }, 250);
-// setInterval(setTime, 500);
 
 var req = new XMLHttpRequest();
 
